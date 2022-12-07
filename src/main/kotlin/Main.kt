533666da5f7069625020388dda7314dd110bb4d4
MarutSharma <marutsharma007@gmail.com>
2022-12-07 16:47:14 +0530

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Person(val name:String , var age:Int = 18){
+    fun info(){
+        println("Name is $name")
+        println("Age is $age")
+    }
+}
+fun main(){
+    val p1 = Person("raju")
+    val p2 = Person("rani",19)
+    p1.info()
+    p2.info()
+    p1.age = 21
+    p1.info()
 }
--- src/main/kotlin/try.kt
+++ src/main/kotlin/try.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World")
+    val border = "`-._,-'"
+    val timesToRepeat = 4
+    printBorder(border, timesToRepeat)
+    println("  Happy Birthday, Jhansi!")
+    printBorder(border, timesToRepeat)
+}
+
+fun printBorder(border: String, timesToRepeat: Int) {
+    repeat(timesToRepeat) {
+        print(border)
+    }
+    println()
 }
